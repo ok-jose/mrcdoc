c1f183aa89ba7fb2ec6083f1e9e3112c868132d3
Jose Lee <Jose Lee>
2017-03-11 18:33:26 +0800

    first

---
+++ src/components/Hello.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+      <!-- quill-editor -->
+      <quill-editor ref="myTextEditor"
+                    v-model="content"
+                    :config="editorOption"
+                    @blur="onEditorBlur($event)"
+                    @focus="onEditorFocus($event)"
+                    @ready="onEditorReady($event)">
+      </quill-editor>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        name: 'base-example',
+        content: '<h2>I am Example</h2>',
+        editorOption: {}
+      }
+    },
+    methods: {
+      onEditorBlur (editor) {
+        console.log('editor blur!', editor)
+      },
+      onEditorFocus (editor) {
+        console.log('editor focus!', editor)
+      },
+      onEditorReady (editor) {
+        console.log('editor ready!', editor)
+      }
+    },
+    computed: {
+      editor () {
+        return this.$refs.myTextEditor.quillEditor
+      }
+    },
+    mounted () {
+      console.log('this is my editor', this.editor)
+      setTimeout(() => {
+        this.content = '<h1>i am changed!</h1>'
+      }, 1800)
+    }
+  }
+</script>
+
+<style>
+  .ql-container .ql-editor {
+    min-height: 40em;
+    padding-bottom: 1em;
+    max-height: 25em;
+  }
+
+  .html {
+    height: 9em;
+    overflow-y: auto;
+    border: 1px solid #ccc;
+    border-top: none;
+    resize: vertical;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/team',
+      name: 'hello',
+      component: Hello
+    }
+  ]
+})
